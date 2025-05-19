904a3fa18d1097e9658a280de3c1da28e9fca7fc
Utkarsh Krishnat Buva <nyxeos@localhost.localdomain>
2025-05-19 23:45:12 +0530

    Made changes

--- cohortWeb2/week2/week2.2/http_server/index.js
+++ cohortWeb2/week2/week2.2/http_server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000               // listening on port 3000
 
 app.get('/', function(req, res){
-    res.send("Hello world")
+    res.send("Hello World !")
 })
 
 app.post('/conversations', function(req, res){
