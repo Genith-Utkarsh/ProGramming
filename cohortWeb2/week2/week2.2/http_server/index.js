e9be2815f76f8b2292a17fdfc647bde068150432
Utkarsh Krishnat Buva <nyxeos@localhost.localdomain>
2025-05-20 09:49:20 +0530

    Updated express files

--- cohortWeb2/week2/week2.2/http_server/index.js
+++ cohortWeb2/week2/week2.2/http_server/index.js
@@ -1,17 +1,25 @@
 const express = require("express")     // Importing Express library
+const bodyParser = require("body-parser")   // Importing body-parser library
 const app = express()
 const port = 3000               // listening on port 3000
 
-app.get('/', function(req, res){
-    res.send("Hello World !")
+app.use(bodyParser.json())   // Middlewares
+
+// app.get('/', function(req, res){
+//     res.send("Hello World !")
+// })
+
+app.post('/', function(req, res){
+    console.log(req.body)
 })
 
-app.post('/conversations', function(req, res){
-    console.log(req.headers["authorization"])
-    res.send({
-        msg : "2 + 2 = 4"
-    })
-})
+
+// app.post('/conversations', function(req, res){
+//     console.log(req.headers["authorization"])
+//     res.send({
+//         msg : "2 + 2 = 4"
+//     })
+// })
 
 app.get('/another', function(req, res){
     res.json({
