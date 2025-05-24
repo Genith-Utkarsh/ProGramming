0087568f3f2727e858956fd61c199ddd53cb7bd3
Utkarsh <buvautkarsh849@gmail.com>
2025-05-24 08:56:49 +0000

    added some files

---
+++ cohortWeb2/week3/week3.1/hospital/basicHospital.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get("/health-checkup", (req, res) => {
+    // HEALTH CHECKS   
+    const kidneyId = req.query.kidneyId
+    const username = req.headers.username
+    const password = req.headers.password
+
+
+    if(username != "utkarsh" || password != "pass")
+    {
+        res.status(403).json({
+            msg  : "User does not exist"
+        })
+        return
+    }
+
+    if(kidneyId != 2 || kidneyId != 1 ){
+        res.status(411).json({
+            msg : "wrong inputs"
+        })  
+        return 
+      }
+})
+
+app.listen(port, function(){
+    console.log(`This app is listening  on port ${port}`)
+})
