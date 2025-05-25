a07a6558df349969930b6fa72129ad00a7589801
Utkarsh <buvautkarsh849@gmail.com>
2025-05-25 10:19:13 +0000

    added numberOfRequests.js

---
+++ cohortWeb2/week3/week3.1/middleWares/numberRequests.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+let numberOfReq = 0
+
+function calculateReq(req, res, next){
+    numberOfReq++
+    console.log(numberOfReq)
+    next()
+}
+
+app.get("/", calculateReq, function(req, res){
+    res.send("Done")
+})
+
+
+app.listen(port, () =>{
+    console.log(`This app is running on port ${port}`)
+})
