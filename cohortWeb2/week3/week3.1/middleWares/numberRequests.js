9516460cd34786458fce18dd208b5de73c52f283
Utkarsh <buvautkarsh849@gmail.com>
2025-05-25 10:32:51 +0000

    changed some file

--- cohortWeb2/week3/week3.1/middleWares/numberRequests.js
+++ cohortWeb2/week3/week3.1/middleWares/numberRequests.js
@@ -2,6 +2,11 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+
+// if middleware ko har route me cal krna hai to use app.use
+
+
+
 let numberOfReq = 0
 
 function calculateReq(req, res, next){
@@ -10,10 +15,19 @@ function calculateReq(req, res, next){
     next()
 }
 
-app.get("/", calculateReq, function(req, res){
-    res.send("Done", numberOfReq)
+app.use(calculateReq)
+
+// app.get("/", calculateReq, function(req, res){
+//     res.send("Done", numberOfReq)
+// })
+
+app.get("/health-check", function(req, res){
+    res.send("Done")
 })
 
+app.get("/health-check2", function(req, res){
+    res.send("Done2")
+})
 
 app.listen(port, () =>{
     console.log(`This app is running on port ${port}`)
