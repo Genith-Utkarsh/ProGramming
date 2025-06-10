a5d7cf8f99e88087c409e77ded2ab8c0f47af810
Utkarsh <buvautkarsh849@gmail.com>
2025-06-10 18:30:49 +0000

    baisc valdation

---
+++ cohortWeb2/week3/week3.1/zodAuth/basicValidation.js
@@ -0,0 +1,12 @@
+const zod = require("zod")
+
+function validateInpute(arr)
+{
+    const schema = zod.array(zod.number)
+
+    const responce = schema.safeParse(arr)
+
+    console.log(responce)
+}
+
+validateInpute([1, 2, 3])
