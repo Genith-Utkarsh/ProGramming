baa06f0e17735984f72e1f98ce5e475b94095e56
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-06-23 16:53:15 +0530

    started react

--- cohortWeb2/week5/ReactBasics/part1/ytReacty/src/App.jsx
+++ cohortWeb2/week5/ReactBasics/part1/ytReacty/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 function App(){
   const name = "koko"
 
@@ -5,10 +7,15 @@ function App(){
     console.log("Hello")
   }
 
+  const [user, setUser] = useState("Shadow")
 
+  function changeUser() {
+    setUser("Jinwoo sung")
+  }
   return <div>
     <h1>Hello {user} </h1>
     <button onClick={clickMe}>Click buddy</button>
+    <button onClick={changeUser}>Change User</button>
   </div>
 }
 
