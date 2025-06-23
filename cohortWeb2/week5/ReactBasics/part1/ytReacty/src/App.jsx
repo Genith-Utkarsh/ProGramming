76a6ba23565e9db21ff8bcb1ff321afa651a1dc3
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-06-23 16:44:02 +0530

    started react

--- cohortWeb2/week5/ReactBasics/part1/ytReacty/src/App.jsx
+++ cohortWeb2/week5/ReactBasics/part1/ytReacty/src/App.jsx
@@ -5,16 +5,10 @@ function App(){
     console.log("Hello")
   }
 
-  let user = "Shadow"
-
-  function changeUser(){
-    user = "Jinwoo Sung"
-  }
 
   return <div>
     <h1>Hello {user} </h1>
     <button onClick={clickMe}>Click buddy</button>
-    <button onClick={changeUser}>Chnge user</button>
   </div>
 }
 
