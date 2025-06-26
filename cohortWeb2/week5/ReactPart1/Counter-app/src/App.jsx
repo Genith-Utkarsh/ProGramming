3d70ccc5a68d38c802c70bc36e872dd9759834e3
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-06-26 16:47:03 +0530

    heheheheh

--- cohortWeb2/week5/ReactPart1/Counter-app/src/App.jsx
+++ cohortWeb2/week5/ReactPart1/Counter-app/src/App.jsx
@@ -24,28 +24,76 @@ import './App.css'
 
 
 
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+//       <CustomButton count = {count} setCount = {setCount} ></CustomButton>  
+//       <CustomButton count = {count + 1} setCount = {setCount} ></CustomButton> 
+//       <CustomButton count = {count - 1} setCount = {setCount} ></CustomButton> 
+
+//     </div>
+//   )
+// }
+
+
+// function CustomButton(props){
+
+//   function OnclickHandler() {
+//     props.setCount(props.count + 1)}
+
+//   return <button onClick={OnclickHandler}>
+//     Counter = {props.count}
+//   </button>
+// }
+
+// ================================================================
+
+
+// Todo app dumb way
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+        title : "Gp to gym",
+        description : "6pm to 7pm"
+  }, 
+  {
+      title : "GO to study",
+      description : "8pm to 10 pm"
+  }
+])
+
+
+  function AddTodo(){
+    setTodos([...todos, {
+      title : "Random title",
+      description : "Rndom Desciption"
+    }])
+  }
 
   return (
     <div>
-      <CustomButton count = {count} setCount = {setCount} ></CustomButton>  
-      <CustomButton count = {count + 1} setCount = {setCount} ></CustomButton> 
-      <CustomButton count = {count - 1} setCount = {setCount} ></CustomButton> 
 
+      <button onClick={AddTodo}>Add random todo</button>
+      {/* <Todo title = {todos[0].title} description = {todos[0].description} />
+      <Todo title = {todos[1].title} description = {todos[1].description} /> */}
+
+
+      {
+        todos.map(function(todo){
+          return <Todo title = {todo.title} description = {todo.description} />
+        })
+      }
     </div>
   )
 }
 
-
-function CustomButton(props){
-
-  function OnclickHandler() {
-    props.setCount(props.count + 1)}
-
-  return <button onClick={OnclickHandler}>
-    Counter = {props.count}
-  </button>
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
 }
 
 export default App
