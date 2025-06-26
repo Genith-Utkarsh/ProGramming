523765a10c863f7e4ad88bf524b03bde734d645a
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-06-26 14:45:26 +0530

    heheheheh

--- cohortWeb2/week5/ReactPart1/Counter-app/src/App.jsx
+++ cohortWeb2/week5/ReactPart1/Counter-app/src/App.jsx
@@ -3,24 +3,49 @@ import './App.css'
 
 
 
+// function App() {
+
+//   const [count, setCount] = useState(0)          // Given state Variable something which is Dynamic
+
+
+//   function clickHandler(){
+//     setCount(count + 1)
+//     // because state updates are asynchronous in React.
+
+//     // Updating the state on clcick of button
+//   }
+
+//   return (
+//     <div>  
+//       <button onClick={clickHandler}>Total count = {count}</button>
+//     </div>
+//   )
+// }
+
+
+
 function App() {
-
-  const [count, setCount] = useState(0)          // Given state Variable something which is Dynamic
-
-
-  function clickHandler(){
-    setCount(count + 1)
-    // because state updates are asynchronous in React.
-
-    // Updating the state on clcick of button
-  }
+  const [count, setCount] = useState(0)
 
   return (
-    <div>  
-      <button onClick={clickHandler}>Total count = {count}</button>
+    <div>
+      <CustomButton count = {count} setCount = {setCount} ></CustomButton>
     </div>
   )
 }
 
+
+function CustomButton(props){
+
+  function OnclickHandler() {
+    props.setCount(props.count + 1)}
+
+  return <button onClick={OnclickHandler}>
+    Counter = {props.count}
+  </button>
+}
+
 export default App
+
+
 
