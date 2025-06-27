deb762b7bddbc52321ab468673f50e0fc8226753
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-06-27 16:49:54 +0530

    week1.2 revision

--- cohortWeb2/week1/week1.2/array.js
+++ cohortWeb2/week1/week1.2/array.js
@@ -32,3 +32,30 @@ for(let i = 0; i < arrNum.length ; i++)
 }
 
 console.log("Biggest Number in the array : ", bigNum);
+
+
+
+
+
+
+
+// Array Methods
+// 1. push() - adds an element to the end of the array
+// 2. pop() - removes the last element from the array
+// 3. unshift() - adds an element to the beginning of the array
+// 4. shift() - removes the first element from the array
+// 5. splice() - adds or removes elements from the array            
+// 6. slice() - returns a shallow copy of a portion of an array
+// 7. forEach() - executes a provided function once for each array element
+
+
+
+const arr = [1, 2, 3, 4, 5];
+arr.push(6)
+arr.unshift(0)
+arr.shift()
+arr.pop()
+console.log(arr);
+
+const newArr = arr.map(element => element * 2 )// This will create a new array with each element multiplied by 2)
+
