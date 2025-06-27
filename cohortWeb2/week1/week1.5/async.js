6c179267225abafd04cda608e2033d25768bd03e
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-06-28 00:23:15 +0530

    week1.3 revision

--- cohortWeb2/week1/week1.5/async.js
+++ cohortWeb2/week1/week1.5/async.js
@@ -1,12 +1,16 @@
 
-console.log("Before async function")
-function displayHello()
-{
+
+
+console.log("Step 1 Before async functon")          // 1
+const displayHello = () => {
     console.log("Hello")
 }
 
-setTimeout(displayHello, 3000)   // when the thread become idle the it will be called 
+setTimeout(displayHello, 2000)  // 2 milisecond              // 3
 
-console.log("After async function");
+console.log("Step last After async function")           // 2
 
 
+
+
+/// setTimeout => we need pass a function and time in milliseconds as parameters
--- cohortWeb2/week1/week1.5/fileSys.js
+++ cohortWeb2/week1/week1.5/fileSys.js
@@ -1,9 +1,39 @@
-const fs = require("fs")   // file system module
+const { error } = require("console");
+const fs = require("fs")       // importing a fs  module
 
-fs.readFile("a.txt", "utf-8", function(err, data)               // async function
-{
-    console.log(data)
+
+console.log(" 1)   Before async function")
+
+// Async function
+fs.readFile("a.txt", "utf-8", (err, data) =>{
+    if(err){
+        console.log("2)    Error reading file:", err);
+        return;                
+    }
+    console.log("2)    File content : ", data)
 })
 
 
-console.log("Hey there")
+console.log("3)    After file read async function")
+
+
+
+
+// Read file synchro.......
+
+
+const data  = fs.readFileSync("a.txt", "utf-8")
+console.log(data)
+
+
+
+
+// Write FIle
+
+
+fs.writeFile("a.txt", "I changed the file content like hacker ....", (err) => {
+    if (err) throw err
+    else {
+        console.log("FIle written")
+    }
+})
