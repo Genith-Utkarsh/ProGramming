28fe82d310b7412f81834d9ba7cd9076605260b9
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-06-29 17:01:05 +0530

    started week 2.2

---
+++ cohortWeb2/week2/week2.2/http_server/PracticeHttp.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.json()) // Middleware to parse JSON bodies
+
+
+app.get("/", (req, res) => {
+
+    res.send("Welcome to the HTTP Server!")
+
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
