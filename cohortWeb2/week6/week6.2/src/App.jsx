31babf7edfe8e1030211fb0300ff31b7a4283764
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-13 22:48:13 +0530

    learned some more

--- cohortWeb2/week6/week6.2/src/App.jsx
+++ cohortWeb2/week6/week6.2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import axios from "axios"
@@ -38,36 +38,124 @@ import './App.css'
 // gETTing todo by its id
 
 
-function App(){
-  const [id, setId] = useState(1)
+// function App(){
+//   const [id, setId] = useState(1)
 
-  function genId(){
-    setId(Math.floor(Math.random() * 4 + 1) )
-  }
+//   function genId(){
+//     setId(Math.floor(Math.random() * 4 + 1) )
+//   }
 
-  return (
-    <div>
-      <button onClick={genId}>Get Todo</button>
-      <Todo id={id} />
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <button onClick={genId}>Get Todo</button>
+//       <Todo id={id} />
+//     </div>
+//   )
+// }
 
-function Todo({id}){
-  const [todos, setTodos] = useState([])
+// function Todo({id}){
+//   const [todos, setTodos] = useState([])
 
-  useEffect(()=>{
-    axios.get(`https://expresstodobackend.onrender.com/todos/${id}`)
-    .then((responce) => {
-      setTodos(responce.data)
-    })
-  }, [id])
+//   useEffect(()=>{
+//     axios.get(`https://expresstodobackend.onrender.com/todos/${id}`)
+//     .then((responce) => {
+//       setTodos(responce.data)
+//     })
+//   }, [id])
 
-  return <div>
-    <h1>{todos.title}</h1>
-    <h5>{todos.description}</h5>
-  </div>
+//   return <div>
+//     <h1>{todos.title}</h1>
+//     <h5>{todos.description}</h5>
+//   </div>
 
-}
+// }
+
+// =====================================================
+
+// function App(){
+//   const [id, setId] = useState()
+
+//   return (
+//     <>
+//     <button onClick={()=>{
+//       setId(1)
+//     }}>1</button>
+//     <button onClick={() => {
+//       setId(2)
+//     }}>2</button>
+//     <button onClick={()=>{
+//       setId(3)
+//     }}>3</button>
+//     <button onClick={() =>{
+//       setId(4)
+//     }}>4</button>
+//     <button onClick={() =>{
+//       setId(5)
+//     }}>5</button>
+    
+//     <Display  id={id}/>
+    
+
+//     </>
+//   )
+
+// }
+
+// function Display({id}){
+//   const [todos, setTodos] = useState([])
+
+//   useEffect(() => {
+//     axios.get(`https://expresstodobackend.onrender.com/todos/${id}`)
+//     .then((responce) => {
+//       setTodos(responce.data)
+//     })
+//   }, [id])
+
+//   return <div>
+//     <h1>{todos.title}</h1>
+//     <h5>{todos.description}</h5>
+//   </div>
+// }
+
+
+//================================================
+
+
+// function App(){
+//   const [counter, setCounter] = useState(0)
+//   const [inputValue, setInputValue] = useState(1)
+  
+
+//   let sum = useMemo(() => {
+//     let sum = 0
+//     for(let i = 1 ; i <= inputValue ; i++ ){
+//       sum += i
+//     }
+//     return sum
+//   }, [inputValue])
+
+//   return( <div>
+//     <input placeholder='Enter number' onChange={function(e) {
+//       setInputValue(e.target.value) 
+//     }} />
+
+//     <br />
+
+//     <div> `The sum from 1 till {inputValue} is {sum}`</div>
+//     <br />
+
+//     <button onClick={() => {
+//       setCounter(counter + 1)
+//     }}>Counter is {counter}</button>
+
+//   </div>
+//   )
+// }
+
+
+// ==========================================================
+
+
+
 
 export default App
