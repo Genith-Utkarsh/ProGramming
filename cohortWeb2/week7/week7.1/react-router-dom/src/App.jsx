709ea30a45d4cd1d476f7bd9479066856d8a4ed6
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-15 21:03:47 +0530

    learned about routing

--- cohortWeb2/week7/week7.1/react-router-dom/src/App.jsx
+++ cohortWeb2/week7/week7.1/react-router-dom/src/App.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Landing} from './components/Landing';
-import {Dashboard} from './components/Dashboard';
+import React from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Landing } from "./components/Landing";
+import { Dashboard } from "./components/Dashboard";
 
 function App() {
-
+  
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route  path = "/" element = {<Landing />} />
-        <Route path = "/dashboard" element = {<Dashboard /> } />
-      </Routes>
-    </BrowserRouter>
-  )
+    <div>
+      
+      <BrowserRouter>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+function AppBar(){
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <div>
+        <button onClick={() => {
+          navigate("/")
+        }}>Landing</button>
+        <button onClick={() => {
+          navigate("/dashboard")
+        }}>Dashboard</button>
+
+      </div>
+    </>
+  )
+}
+export default App;
