7386369de13a471890246188638ed28261dc1cba
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-18 11:55:18 +0530

    Recoil deep dive basics

---
+++ cohortWeb2/week7/week7.4/RecoilDeepDive/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <button></button>
+    <button></button>
+    <button></button>
+    <button></button>
+    <button></button>
+      </>
+  )
+}
+
+export default App
