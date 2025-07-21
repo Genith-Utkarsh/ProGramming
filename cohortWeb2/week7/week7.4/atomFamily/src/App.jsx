fa056fe8fa00d28fe3fa8e787bbf48d6b796814a
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-21 14:37:07 +0530

    atom family updatinf todo task

--- cohortWeb2/week7/week7.4/atomFamily/src/App.jsx
+++ cohortWeb2/week7/week7.4/atomFamily/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useRecoilValue , RecoilRoot} from 'recoil'
+import { useRecoilValue , RecoilRoot, useSetRecoilState} from 'recoil'
 import { todoAtomFamily } from './atom'
 
 function App() {
@@ -10,12 +10,29 @@ function App() {
   return (
     <>
       <RecoilRoot>
+        <TodoUpdater />
         <Todo id = {1} />
         <Todo id ={2} />
+        <Todo id={2} />
 
       </RecoilRoot>
     </>
   )
+}
+
+
+function TodoUpdater(){
+  const updateCurrentTodo = useSetRecoilState(todoAtomFamily(2))
+
+  useEffect(() => {
+    setTimeout(() =>{
+      updateCurrentTodo({
+        id : 2,
+        title : "Updating",
+        description : "Todo 2 has been updated"
+      })
+    }, 5000)
+  }, [])
 }
 
 
