d8c22dd64b864201f0fab839f8a43c499647873e
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-21 15:47:01 +0530

    Recoil Loadable

--- cohortWeb2/week7/week7.4/atomFamily/src/App.jsx
+++ cohortWeb2/week7/week7.4/atomFamily/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useRecoilValue , RecoilRoot, useSetRecoilState} from 'recoil'
+import { useRecoilValue , RecoilRoot, useSetRecoilState, useRecoilValueLoadable} from 'recoil'
 import { todoAtomFamily } from './atom'
 
 function App() {
@@ -14,6 +14,8 @@ function App() {
         <Todo id = {1} />
         <Todo id ={2} />
         <Todo id={2} />
+
+        <TodoLodable id = {3} />
 
       </RecoilRoot>
     </>
@@ -44,6 +46,25 @@ function Todo({id}){
   </div>
 }
 
+
+function TodoLodable({id}){
+  const todo = useRecoilValueLoadable(todoAtomFamily(id))
+
+  // This todo will have contents, state , hasError, hasValue
+  // contents will have the data
+  // state will have the state of the data (loading, hasValue, hasError)
+
+  if(todo.state === "loading"){
+    return <div>Loading....</div>
+  } else if(todo.state === "hasError"){
+    return <div>Error loading todo</div>
+  } else {
+    return <div>
+      <h1>{todo.contents.title}</h1>
+      <h4>{todo.contents.description}</h4>
+    </div>
+  }
+}
 
 
 export default App
