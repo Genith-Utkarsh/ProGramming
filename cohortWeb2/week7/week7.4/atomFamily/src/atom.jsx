93e00b7ce97249085e09d53d50341beaaff300a2
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-21 15:26:42 +0530

    Selector Family

--- cohortWeb2/week7/week7.4/atomFamily/src/atom.jsx
+++ cohortWeb2/week7/week7.4/atomFamily/src/atom.jsx
@@ -1,9 +1,23 @@
-import { atomFamily } from 'recoil'
+import { atomFamily, selectorFamily } from 'recoil'
 import { TODOS } from './todo.jsx'
+import axios from 'axios'
+
+// export const todoAtomFamily = atomFamily({
+//     key: "todoAtomFamily",
+//     default: (id) => {
+//         return TODOS.find(x => x.id === id)
+//     }
+// })
+
 
 export const todoAtomFamily = atomFamily({
-    key: "todoAtomFamily",
-    default: (id) => {
-        return TODOS.find(x => x.id === id)
-    }
+    key : "todoAtomFamily" ,
+    default : selectorFamily({
+        key : "todoSelectorFamily",
+        get : (id) => async  ({get}) =>{
+            const responce = await axios.get(`https://expresstodobackend.onrender.com/todos/${id}`)
+
+            return responce.data
+        }
+    })
 })
