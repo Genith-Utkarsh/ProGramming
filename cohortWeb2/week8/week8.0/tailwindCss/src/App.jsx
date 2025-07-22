c5da032fc93ae37f1c04c1b0016dea8a01d1e6c1
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-22 13:01:53 +0530

    Learned new

--- cohortWeb2/week8/week8.0/tailwindCss/src/App.jsx
+++ cohortWeb2/week8/week8.0/tailwindCss/src/App.jsx
@@ -27,16 +27,76 @@ import "./App.css";
 
 // Tailwind
 
+// function App() {
+//   return (
+//     <>
+//       <div className="flex justify-between">
+//         <div>Box 1</div>
+//         <div>Box 2</div>
+//         <div>Box 3</div>
+
+//       </div>
+//     </>
+//   )
+// }
+
+
+//Equal width with cols
+// function App() {
+//   return (
+//     <>
+//       <div className="grid grid-cols-3">
+//         <div style={{background :"green"}}>Box 1</div>
+//         <div style={{background : "blue"}}>Box 2</div>
+//         <div style={{background : "red"}}>Box 3</div>
+
+//       </div>
+//     </>
+//   )
+// }
+
+
+
+// function App() {
+//   return (
+//     <>
+//       <div className="grid grid-cols-12">
+//         <div className="col-span-3  bg-green-500">Box 1</div>
+//         <div className="col-span-7 bg-red-500">Box 2</div>
+//         <div className="col-span-2 bg-yellow-500" >Box 3</div>
+
+//       </div>
+//     </>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <div className="grid grid-cols-12">
+//         <div className="col-span-4  md: col-span-3 ">Box 1</div>
+//         <div className="col-span-4  md: col-span-8">Box 2</div>
+//         <div className="col-span-4  md : col-span-1">Box 3</div>
+
+//       </div>
+//     </>
+//   )
+// }
+
+
+// background and fonts
+
+
+
 function App() {
   return (
     <>
-      <div className="flex justify-between">
-        <div className="bg-green-500 p-4">Box 1</div>
-        <div className="bg-red-500 p-4">Box 2</div>
-        <div className="bg-blue-500 p-4">Box 3</div>
+      <div className="bg-black" >
+        <div className="text-white">Hey There</div>
       </div>
     </>
   )
 }
+
 
 export default App;
