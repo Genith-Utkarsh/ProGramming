cd3c372e3cf802cc4b05f2e5e82714eea5db44ec
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-22 15:21:35 +0530

    Fixed/improved sign in button

--- cohortWeb2/week8/week8.0/YoutubeClone/src/Components/AppBar.jsx
+++ cohortWeb2/week8/week8.0/YoutubeClone/src/Components/AppBar.jsx
@@ -4,6 +4,8 @@ export function AppBar() {
     return <div className="flex justify-around text-white pt-2">
         <div className="text-md inline-flex items-center">Youtube</div>
         <SearchBar />
-        <div>Sign IN</div>
+        <div>
+            <button  className="bg-slate-600 py-2 px-3 inline-flex items-center  rounded-2xl ">Sign In</button>
+        </div>
     </div>
 }
