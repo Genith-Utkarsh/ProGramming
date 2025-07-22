33bda62a192ad95627667b6b189fb80a55437524
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-22 15:15:03 +0530

    Fixed the searched bar

--- cohortWeb2/week8/week8.0/YoutubeClone/src/Components/AppBar.jsx
+++ cohortWeb2/week8/week8.0/YoutubeClone/src/Components/AppBar.jsx
@@ -1,8 +1,8 @@
 import { SearchBar } from "./SearchBar";
 
 export function AppBar() {
-    return <div className="flex justify-between text-white">
-        <div>Yt Logo</div>
+    return <div className="flex justify-around text-white pt-2">
+        <div className="text-md inline-flex items-center">Youtube</div>
         <SearchBar />
         <div>Sign IN</div>
     </div>
--- cohortWeb2/week8/week8.0/YoutubeClone/src/Components/SearchBar.jsx
+++ cohortWeb2/week8/week8.0/YoutubeClone/src/Components/SearchBar.jsx
@@ -1,33 +1,30 @@
 export const SearchBar = () => {
   return (
-    <div className="w-2/4">
-      <form>
-        <div className="w-96 flex text-gray-900 border border-gray-500 rounded-3xl p-1 pl-3 text-sm pr-2">
-          <input
-            id="default-search"
-            className="w-full bg-slate-950 text-white border-none outline-none"
-            placeholder="Search"
-            required
-          />
-          <button className="bg-slate-950 text-gray-400 font-bold py-2 px-4 rounded inline-flex items-center">
-            <svg
-              className="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 8 7a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </button>
-        </div>
-      </form>
+    <div>
+      <div className="w-96 flex  border border-gray-500 rounded-3xl p-1 pl-3 text-sm pr-2">
+        <input
+          id="default-search"
+          className="w-full bg-slate-950 text-white border-none outline-none"
+          placeholder="Search"
+          required
+        />
+        <button className="bg-slate-950 text-gray-400 font-bold py-2 px-4 rounded inline-flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
