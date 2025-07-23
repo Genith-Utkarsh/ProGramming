de6aca275ea1bd5d17f00fb920538e45ea2c0b3d
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-23 18:32:52 +0530

    Solved problems

---
+++ demo cpp/pw/lecture4/Rectangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows =4 , cols = 3;
+    for(int i = 1 ; i <= 4 ; i++){
+        for(int j = 1; j <= 3 ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
