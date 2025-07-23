0d315bd876d2fc3c6472d597d35126f18333098e
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-23 19:12:18 +0530

    Library functions

---
+++ demo cpp/pw/lecture5/2.LibraryFn.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<sqrt(2)<<endl;
+    cout<<cbrt(32)<<endl;
+    cout<< min(4, 8) << endl;
+    cout << max(4, 8) << endl;
+    cout<< pow(2, 3) << endl;
+}
