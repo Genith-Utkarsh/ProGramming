4f6065d9b2fd94aa1a1d3602ea27eb2470a758af
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-23 15:52:06 +0530

    Added revencure card

--- cohortWeb2/week8/week8.1/DukanBasicClone/src/App.jsx
+++ cohortWeb2/week8/week8.1/DukanBasicClone/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import { RevenueCard } from './Components/RevenueCard'
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div className='grid grid-cols-3' >
+        <RevenueCard title={"Amount pending"} orderCount={32} amount={92412} />
+    </div>
   )
 }
 
---
+++ cohortWeb2/week8/week8.1/DukanBasicClone/src/Components/RevenueCard.jsx
@@ -0,0 +1,33 @@
+export const RevenueCard = ({ title, showWarning, orderCount, amount }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4">
+      <div className="flex">
+        <div className="text-gray-700  ">{title}</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+          />
+        </svg>
+      </div>
+      <div className="flex justify-between">
+        <div>Rs {amount}</div>
+        <div>
+          {orderCount ? (
+            <div>
+              {orderCount} {">"}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
