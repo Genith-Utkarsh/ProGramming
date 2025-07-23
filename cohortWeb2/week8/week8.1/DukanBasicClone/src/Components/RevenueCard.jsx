b5412565393b1dd299273c9d23ebbea12ff3f1d3
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-23 16:24:46 +0530

    Made some changes

--- cohortWeb2/week8/week8.1/DukanBasicClone/src/Components/RevenueCard.jsx
+++ cohortWeb2/week8/week8.1/DukanBasicClone/src/Components/RevenueCard.jsx
@@ -3,13 +3,14 @@ export const RevenueCard = ({ title, showWarning, orderCount, amount }) => {
     <div className="bg-white rounded-lg shadow-md p-4">
       <div className="flex">
         <div className="text-gray-700  ">{title}</div>
-        <svg
+        <div className="flex justify-center flex-col ml-2 ">
+            <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="size-6"
+          className="size-5"
         >
           <path
             strokeLinecap="round"
@@ -17,13 +18,32 @@ export const RevenueCard = ({ title, showWarning, orderCount, amount }) => {
             d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
           />
         </svg>
+        </div>
       </div>
-      <div className="flex justify-between">
-        <div>Rs {amount}</div>
+      <div className="flex justify-between p-2">
+        <div className="text-4xl font-semibold flex items-center">
+          Rs {amount}
+        </div>
         <div>
           {orderCount ? (
-            <div>
-              {orderCount} {">"}
+            <div className="flex  cursor-pointer ">
+              <div className="text-blue-500 underline font-medium">{orderCount} Order(s)</div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 fill-blue-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </div>
             </div>
           ) : null}
         </div>
