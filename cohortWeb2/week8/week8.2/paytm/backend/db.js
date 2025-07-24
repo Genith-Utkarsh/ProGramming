3058fee13d1332d9bc560e206c6fcc02d6101163
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-24 14:37:32 +0530

    Phase 1

---
+++ cohortWeb2/week8/week8.2/paytm/backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+
+const userSchema = new mongoose.Schema({
+    username : String,
+    password : String,
+    firstName : String,
+    lastName  : String
+})
+
+const User = mongoose.model("User", userSchema)
