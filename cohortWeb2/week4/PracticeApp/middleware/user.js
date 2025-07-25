9fdd24dfe98156447cfbe765804fbeb3e41b7fc2
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-25 12:38:00 +0530

    FIxed user middleware

--- cohortWeb2/week4/PracticeApp/middleware/user.js
+++ cohortWeb2/week4/PracticeApp/middleware/user.js
@@ -1,4 +1,21 @@
+const { User } = require("../db/db")
+
 const userAuthMiddleWare = (req, res, next) => {
+    const username = req.headers.username
+    const password = req.headers.password
+
+    User.findOne({username}).then((user) => {
+        if(username !== user.admin){
+            return res.send("User not found in our database")
+        }
+        if(password != user.password){
+            return res.send("Incorrect password")
+        }
+        next()
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).send("Internal server error")
+    })
 
 }
 
