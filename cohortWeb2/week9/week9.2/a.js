429924c5fc4b9f8fa5009dc28fa73e9b8b859b7b
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-31 17:08:52 +0530

    solved some basic questions

---
+++ cohortWeb2/week9/week9.2/a.js
@@ -0,0 +1,13 @@
+"use strict";
+// const x: number = 2
+// console.log(x)
+// function greet(firstName : string, lastName : string, age : number, income : any){
+//     console.log("Hello" + firstName)
+// }
+// greet("Alan", "Wallker", 20, 20000000000000)
+// sum of two numbers 
+function sum(a, b) {
+    return a + b;
+}
+const value = sum;
+console.log(value);
