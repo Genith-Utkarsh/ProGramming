21d8ee4ea9f3b68bd07a9a8a93b4e4f7e2092a75
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-31 22:53:39 +0530

    changed/ added some code

---
+++ cohortWeb2/week9/week9.2/src/a.ts
@@ -0,0 +1,52 @@
+// const x: number = 2
+// console.log(x)
+
+
+// function greet(firstName : string, lastName : string, age : number, income : any){
+//     console.log("Hello" + firstName)
+// }
+
+// greet("Alan", "Wallker", 20, 20000000000000)
+
+
+
+
+// sum of two numbers 
+
+// function sum(a : number, b: number): number {
+//     return a + b
+// }
+// const value = sum
+// console.log(value)
+
+
+
+// if else 
+
+
+// function isLegal(age: number): boolean {
+//     if(age >= 18){
+//         return true
+//     } else{
+//         return false
+//     }
+// }
+
+// console.log(isLegal(2))
+
+
+
+
+
+// Callback function
+
+// fnName:  (Arguments) => Type for what it returns  eg, void, number, string, boolean etc
+
+
+function runAfter1sec(fnName: () => void ){
+    setTimeout(fnName, 1000)
+}
+
+runAfter1sec(function fnName(){
+    console.log("Hi")
+})
