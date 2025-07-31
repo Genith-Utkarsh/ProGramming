ac4cbca6b65a1db209659af06ab600d746910fb9
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-31 16:02:20 +0530

    Refreshing the dom

--- cohortWeb2/week9/week9.1/src/App.jsx
+++ cohortWeb2/week9/week9.1/src/App.jsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import axios from "axios";
+import "./App.css";
 
 // function App() {
 //   const [render, setRender] = useState(true)
-  
+
 //   useEffect(() => {
 //     const interval = setInterval(() =>{
 //       setRender(render => !render)
@@ -35,8 +36,6 @@ import './App.css'
 //   </div>
 // }
 
-
-
 // function MyComponent(){
 //   useEffect(() => {
 //     console.log("Component mounted")
@@ -51,41 +50,56 @@ import './App.css'
 
 //==========================================================
 
-
 // custom hooks
 // wrapper over actual hooks
 
-
-function useTodos(){
-  const [todos, setTodos] = useState([])
+function useTodos(n) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      axios.get("")
-      .then((res) => {
-        setTodos(res.data)
-      })
-  }, [])
+    const value = setInterval(() => {
+      axios.get("").then((res) => {
+        setTodos(res.data);
+      });
+      setLoading(false)
+    });
 
-  return todos
+    axios.get("").then((res) => {
+      setTodos(res.data);
+    });
+
+    setLoading(false);
+
+    return () => clearInterval(value)
+  }, [n]);
+
+  return { todos, loading };
 }
 
+function App() {
+  const { todos, loading } = useTodos(10);
 
-function App(){
-  const todos = useTodos()
+  if (loading) {
+    return <div>Loading....</div>;
+  }
+
   return (
-    
     <>
-    {todos.map((todo) => <Track todo = {todo} />)}
+      {todos.map((todo) => (
+        <Track todo={todo} />
+      ))}
     </>
-  )
+  );
 }
 
-
-function Track({todo}){
-  return <div>
+function Track({ todo }) {
+  return (
+    <div>
       <h1>todo.title</h1>
       <h4>todo.description</h4>
-  </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
