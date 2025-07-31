afd971229c8d31b2c5a8385190d19288d1a7fe39
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-07-31 14:39:34 +0530

    custom hooks

--- cohortWeb2/week9/week9.1/src/App.jsx
+++ cohortWeb2/week9/week9.1/src/App.jsx
@@ -1,29 +1,90 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [render, setRender] = useState(true)
+  
+//   useEffect(() => {
+//     const interval = setInterval(() =>{
+//       setRender(render => !render)
+//     }, 2000)
 
+//     return () => clearInterval(interval)
+//   }, [])
+
+//   return (
+//     <>
+//       {render ? <MyComponent /> : <div>Hello</div>}
+//     </>
+//   )
+// }
+
+// function MyComponent(){
+//    const [count , setCount] = useState(0)
+
+//     function incrementValue() {
+//       setCount(count + 1 )
+//     }
+
+//   return <div>
+
+//     <div>{count}</div>
+//     <button  onClick={incrementValue}>Increment</button>
+//   </div>
+// }
+
+
+
+// function MyComponent(){
+//   useEffect(() => {
+//     console.log("Component mounted")
+//     return () => {
+//       console.log("Component unmounted")
+//     }
+//   }, [])
+//   return <div>
+//       from inside my MyComponent
+//   </div>
+// }
+
+//==========================================================
+
+
+// custom hooks
+// wrapper over actual hooks
+
+
+function useTodos(){
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+      axios.get("")
+      .then((res) => {
+        setTodos(res.data)
+      })
+  }, [])
+
+  return todos
+}
+
+
+function App(){
+  const todos = useTodos()
   return (
+    
     <>
-      <MyComponent />
+    {todos.map((todo) => <Track todo = {todo} />)}
     </>
   )
 }
 
-function MyComponent(){
-   const [count , setCount] = useState(0)
 
-    function incrementValue() {
-      setCount(count + 1 )
-    }
-    
+function Track({todo}){
   return <div>
-
-    <div>{count}</div>
-    <button  onClick={incrementValue}>Increment</button>
+      <h1>todo.title</h1>
+      <h4>todo.description</h4>
   </div>
 }
 
