221833d82aef51ac3d615c4d75d91dbcbed5a6a1
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-08-02 16:08:29 +0530

    Memo typeScript practice

---
+++ cohortWeb2/week9/week9.2/src/memoPratise.ts
@@ -0,0 +1,33 @@
+// // 1)
+// // type anotation
+
+// let distance: number
+// distance = 12
+// console.log(distance)
+
+
+// let isOnline: boolean
+// isOnline = true
+// console.log(isOnline)
+
+
+// // any
+// let anything: any = "HEy there"
+// console.log(anything)
+
+// anything =12
+// console.log(anything)
+
+
+
+// let city: string = "Kolhapur"
+// let zipCode: number = 416230
+// let isCapital: boolean= false
+
+// console.log(city)
+// console.log(zipCode)
+// console.log(isCapital)
+
+
+
+
