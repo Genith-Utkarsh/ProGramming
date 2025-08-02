946d7f28e7948c797ee1b071301dd9b2074d89b2
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-08-03 00:12:23 +0530

    Assiging values to enums

--- cohortWeb2/week9/week9.3/src/a.ts
+++ cohortWeb2/week9/week9.3/src/a.ts
@@ -1,19 +1,36 @@
+// by default values 
+// enum Direction {
+//   Up, // 1
+//   Down, //2
+//   Right, //3
+//   Left, //4
+// }
+
+// function doSomething(keyPressed: Direction) {
+//   if (keyPressed == Direction.Up) {
+//   }
+// }
+
+// doSomething(Direction.Up);
+// doSomething(Direction.Right);
+
+// console.log(Direction.Up);
+// console.log(Direction.Down);
+// console.log(Direction.Right);
+// console.log(Direction.Left);
+
+
+
+// if you set types  to enums  then you have to set types to all enums
+
 enum Direction {
-  Up, // 1
-  Down, //2
-  Right, //3
-  Left, //4
+    Up = "up",
+    Down = "down",
+    Right = "right",
+    Left = "left"
 }
 
-function doSomething(keyPressed: Direction) {
-  if (keyPressed == Direction.Up) {
-  }
-}
-
-doSomething(Direction.Up);
-doSomething(Direction.Right);
-
-console.log(Direction.Up);
-console.log(Direction.Down);
-console.log(Direction.Right);
-console.log(Direction.Left);
+console.log(Direction.Up)
+console.log(Direction.Down)
+console.log(Direction.Left)
+console.log(Direction.Right)
