66a1c06f8721442ca5c91c6b1400d07e35028331
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-08-06 16:02:00 +0530

    psql

--- cohortWeb2/week10/week10.1/src/index.ts
+++ cohortWeb2/week10/week10.1/src/index.ts
@@ -6,3 +6,20 @@ dotenv.config()
 const client = new Client({
     connectionString : process.env.neonDbStr
 })
+
+
+
+
+async function creatUsersTable() {
+    await client.connect()
+
+    const result = await client.query(`
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                email VARCHAR(50) UNIQUE NOT NULL, 
+                username VARCHAR(255) UNIQUR NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+            )
+        `) 
+}
