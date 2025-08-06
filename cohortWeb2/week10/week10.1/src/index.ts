17cbea0b6b4435fec8273a9890cb2698cac4982a
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-08-06 17:04:49 +0530

    created sample table

--- cohortWeb2/week10/week10.1/src/index.ts
+++ cohortWeb2/week10/week10.1/src/index.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv"
 import { Client } from "pg"
-dotenv.config()
+// dotenv.config()
 
 
 const client = new Client({
-    connectionString : process.env.neonDbStr
+    connectionString : "postgresql://neondb_owner:npg_ByISUEdklQ72@ep-sweet-hill-a53vr6xq-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
 
 
@@ -17,9 +17,23 @@ async function creatUsersTable() {
             CREATE TABLE users (
                 id SERIAL PRIMARY KEY,
                 email VARCHAR(50) UNIQUE NOT NULL, 
-                username VARCHAR(255) UNIQUR NOT NULL,
+                username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
                 created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
             )
         `) 
+        console.log(result)
 }
+
+
+try {
+    const insertQuery = "INSERT INTO users (username, email, password) VALUES ('username2', 'test@gmail.com', 'Hello@123' )"
+    const result = await client.query(insertQuery)
+    console.log("Insertation of data successful",result)
+}
+catch(err){
+    console.log("Error duting insertion", err)
+} finally {
+    client.end()
+}
+creatUsersTable()
