7d1106aa633e79f024c2bbd684420fdcb7edbe86
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-08-09 19:24:28 +0530

    Done type aliases

---
+++ cohortWeb2/week9/week9.2/src/TypeAliases.ts
@@ -0,0 +1,23 @@
+// we use type aliases to give types a name making code cleaner and safer
+
+type Human = {
+    name : string;
+    age: number;
+}
+
+
+// we can assign the custom type to other variable to give it a type
+
+let jacky : Human = {
+    name : "Alan",
+    age : 13
+}
+
+console.log(jacky)
+
+
+type PrcticeSeries = number[]
+
+const stockPrices: PrcticeSeries = [10, 23, 42, 42]
+
+console.log(stockPrices)
